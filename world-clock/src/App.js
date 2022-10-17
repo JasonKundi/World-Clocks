e4589a12be89a70b093612c6fd85fe18e93b2fd0
Jason Kundi <jasonkundi1996@gmail.com>
2022-10-17 15:28:00 +0100

    push

--- world-clock/src/App.js
+++ world-clock/src/App.js
@@ -1,45 +1,33 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Clock from "./components/Clock.jsx"
-import React, {useState, useEffect } from "react";
+import Clock from "./components/Clock.jsx";
+import React, { useState, useEffect } from "react";
 
-function App() {
-  const BASE_URL = 'http://worldtimeapi.org/api/timezone'
-  const URL_LOCATION = findTimeZones(timeZones)
-  let timeZones = ["Europe/London", "America/NewYork", "Asia/Tokyo"]
+export default function App() {
+  const BASE_URL = "http://worldtimeapi.org/api/timezone";
+  let timeZones = ["Europe/London", "America/NewYork", "Asia/Tokyo"];
 
-  useEffect((BASE_URL + URL_LOCATION) => {
-    fetch()
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+  useEffect(() => {
+    for (let i = 0; i < 3; i++) {
+      fetch( BASE_URL + timeZones[i])
+        .then((response) => response.json())
+        .then((data) => (setTimeData(data)));
+    }
   }, []);
-
-  const findTimeZones = (timeZones) => {
-    for (let timezone of timeZones) {
-      return timezone
-
-  }
-
-
 
   const initialTimeData = {
     longHand: "",
     shortHand: "",
-    cityTitle: "" ,
+    cityTitle: "",
     addressLine1: "",
     addressLine2: "",
-  }
-  const [LondonTimeData, setLondonTimeData] = useState(initialTimeData)
-  const [timeData, setTimeData] useState({
-    
-  })
+  };
+  const [LondonTimeData, setLondonTimeData] = useState(initialTimeData);
+  const [timeData, setTimeData]= useState([])
 
   return (
     <div className="App">
-      <Clock 
-      timeData={timeData}/>
+      <Clock timeData={timeData} />
     </div>
   );
 }
-
-export default App;
